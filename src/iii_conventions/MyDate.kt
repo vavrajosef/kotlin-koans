a698cc6f0aedb0ee4d0a01f3287e56ea05ea9411
Josef <vavra.pepa@gmail.com>
2018-11-19 15:12:19 +0100

    Task 28 resolved

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,5 +1,7 @@
 package iii_conventions
 
+import iii_conventions.nextDay
+
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
@@ -20,7 +22,23 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object:Iterator<MyDate> {
+            var next = start
+            override fun hasNext(): Boolean {
+                return next <= endInclusive
+            }
+
+            override fun next(): MyDate {
+                val current = next
+                next = this.next.nextDay()
+                return current
+            }
+
+        }
+    }
+}
 
 operator fun DateRange.contains(lookingFor: MyDate): Boolean
         = lookingFor.compareTo(start) >= 0 && lookingFor.compareTo(endInclusive) <= 0
