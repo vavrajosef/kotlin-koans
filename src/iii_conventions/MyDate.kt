be00384583ed70ffd7d725b2828ec4cc413e8b0c
Josef <vavra.pepa@gmail.com>
2018-11-19 14:36:01 +0100

    Task 25 resolved

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -2,7 +2,17 @@ package iii_conventions
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+
+operator fun MyDate.compareTo(other: MyDate): Int {
+    if(this.year == other.year) {
+        if(this.month == other.month) {
+            return this.dayOfMonth - other.dayOfMonth
+        }
+        return this.month - other.month
+    }
+    return this.year - other.year
+}
 
 enum class TimeInterval {
     DAY,
